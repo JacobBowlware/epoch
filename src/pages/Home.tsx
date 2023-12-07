e0cc876206063147321aabb42197df6fe02a7f38
Jacob Bowlware <jacobbowlware@gmail.com>
2023-12-06 21:57:33 -0600

    Implement basic home UI.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div className="page">
-            <div className="page-section">
-                Home Page
+        <div className="page home">
+            <h1 className="page-section home-header">
+                Get 5 chances to guess a specific year, from 0000 to 2023.
+            </h1>
+            <div className="home-btn__container">
+                <Link to="/play" className="btn home-btn home-btn-primary">Play</Link>
+                <Link to="/instructions" className="btn home-btn">Instructions</Link>
             </div>
+            <ul className="home-footer">
+                <li className="home-footer__item">v1.0.0</li>
+                <li className="home-footer__item">Edited by Jacob Bowlware</li>
+            </ul>
         </div>
     );
 }
